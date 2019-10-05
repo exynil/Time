033cb2f8fb63af1d14634791172057cbe241bf1f
Kim Maxim <exynil@gmail.com>
2019-10-05 19:02:27 +0600

    feature click

--- js/particle.js
+++ js/particle.js
@@ -12,7 +12,6 @@ class Particle {
 			y: Math.random() - 0.5
 		};
 		this.color = color;
-		this.opacity = 0;
 	};
 
 	Update(particles) {
@@ -40,12 +39,64 @@ class Particle {
 	Draw() {
 		ctx.beginPath();
 		ctx.save();
-		ctx.shadowBlur = 25;
+		ctx.shadowBlur = this.radius * 25 / 15;
 		ctx.shadowColor = this.color;
 		ctx.fillStyle = this.color;
 		ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
 		ctx.fill();
 		ctx.restore();
 		ctx.closePath();
+	}
+}
+
+function getDistance(x1, y1, x2, y2) {
+	let xDistance = x2 - x1;
+	let yDistance = y2 - y1;
+
+	return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+}
+
+function rotate(velocity, angle) {
+	const rotatedVelocities = {
+		x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
+		y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
+	};
+
+	return rotatedVelocities;
+}
+
+function resolveCollision(particle, otherParticle) {
+	const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
+	const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
+
+	const xDist = otherParticle.x - particle.x;
+	const yDist = otherParticle.y - particle.y;
+
+	if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+		const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
+
+		const m1 = particle.mass;
+		const m2 = otherParticle.mass;
+
+		const u1 = rotate(particle.velocity, angle);
+		const u2 = rotate(otherParticle.velocity, angle);
+
+		const v1 = {
+			x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
+			y: u1.y
+		};
+		const v2 = {
+			x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
+			y: u2.y
+		};
+
+		const vFinal1 = rotate(v1, -angle);
+		const vFinal2 = rotate(v2, -angle);
+
+		particle.velocity.x = vFinal1.x;
+		particle.velocity.y = vFinal1.y;
+
+		otherParticle.velocity.x = vFinal2.x;
+		otherParticle.velocity.y = vFinal2.y;
 	}
 }
--- js/script.js
+++ js/script.js
@@ -1,32 +1,32 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
 var colors = ['#00FF7F', '#7B68EE', '#00FFFF'];
-var particles;
+var particles = [];
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 
 // Функция для получения кол-ва дней в месяце
-Date.prototype.daysInMonth = function() {
+Date.prototype.daysInMonth = function () {
 	return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
 };
 
 // Функция для получения кол-ва дней в году
-Date.prototype.getDaysInYear = function() {
-	return (new Date(new Date().getFullYear(),11,31) - new Date(new Date().getFullYear(),0,0)) / 86400000;
+Date.prototype.getDaysInYear = function () {
+	return (new Date(new Date().getFullYear(), 11, 31) - new Date(new Date().getFullYear(), 0, 0)) / 86400000;
 }
 
 // Функция для получения кол-ва прошедших дней с начала года
-Date.prototype.getPassedDaysFromBegginigOfTheYear = function() {
-	return (new Date(new Date().getFullYear(),new Date().getMonth(),new Date().getDate()) - new Date(new Date().getFullYear(),0,0))/86400000
+Date.prototype.getPassedDaysFromBegginigOfTheYear = function () {
+	return (new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate()) - new Date(new Date().getFullYear(), 0, 0)) / 86400000
 }
 
 // Функция для прорисовки текста в указанном месте
-function drawText(x, y, text) {
+function drawText(x, y, text, color) {
 	ctx.beginPath();
 	ctx.save();
-	ctx.font = "30pt Rajdhani";
-	ctx.fillStyle = '#F5F5F5';
+	ctx.font = '30pt Bungee Hairline';
+	ctx.fillStyle = color;
 	ctx.fillText(text, x, y);
 	ctx.restore();
 	ctx.closePath();
@@ -37,33 +37,52 @@ function drawProgress(x, y, percent, color) {
 	ctx.beginPath();
 	ctx.save();
 	ctx.shadowBlur = 10;
+	ctx.shadowColor = '#404040';
+	ctx.fillStyle = '#404040';
+	ctx.fillRect(x, y, 350, 3);
 	ctx.shadowColor = color
 	ctx.fillStyle = color;
-	ctx.fillRect(x, y, percent * 3, 3);
+	ctx.fillRect(x, y, percent * 3.5, 2);
 	ctx.restore();
 	ctx.closePath();
 }
 
 // Отслеживание изменения размера окна
-addEventListener('resize', function() {
+addEventListener('resize', function () {
 	canvas.width = innerWidth;
 	canvas.height = innerHeight;
 
 	for (let i = 0; i < particles.length; i++) {
-		if (particles[i].x > canvas.width)
-		{
+		if (particles[i].x > canvas.width) {
 			particles[i].x = randomIntFromRange(particles[i].radius, canvas.width - particles[i].radius);
 		}
-		if (particles[i].y > canvas.height)
-		{
+		if (particles[i].y > canvas.height) {
 			particles[i].y = randomIntFromRange(particles[i].radius, canvas.height - particles[i].radius);
 		}
 	}
 })
 
-function init() {
-	particles = [];
+// Отслеживание клика
+addEventListener('click', function (e) {
+	let numberOfBalls = randomIntFromRange(4, 8);
 
+	for (let i = 0; i < numberOfBalls; i++) {
+		const radius = randomIntFromRange(2, 15);
+		const color = randomColorFromArray(colors);
+		const mass = 1;
+		const speed = 5;
+		const acceleration = 0.1;
+
+		let x = e.pageX;
+		let y = e.pageY;
+
+		particles.push(new Particle(particles.length, radius, mass, speed, acceleration, x, y, color));
+	}
+	console.log(particles);
+})
+
+// Инициализация
+function init() {
 	for (let i = 0; i < 20; i++) {
 		const radius = randomIntFromRange(2, 15);
 		const color = randomColorFromArray(colors);
@@ -101,7 +120,7 @@ function animate() {
 	});
 
 	var marginTop = canvas.height / 2 - 250;
-	var marginLeft = canvas.width / 2 - 150;
+	var marginLeft = canvas.width / 2 - 175;
 
 	// Вычисление процента дня
 	var date = new Date();
@@ -109,7 +128,7 @@ function animate() {
 	var percent = passedSecondsOfDay / 864;
 
 	marginTop += 100;
-	drawText(marginLeft, marginTop, 'Today: ' + percent.toFixed(2) + '%');
+	drawText(marginLeft, marginTop, 'Today: ' + percent.toFixed(2) + '%', '#F5F5F5');
 	marginTop += 20;
 	drawProgress(marginLeft, marginTop, Math.round(percent), colors[0]);
 
@@ -118,7 +137,7 @@ function animate() {
 	percent = passedDaysFromBegginigOfTheOfMonth * 100 / date.daysInMonth();
 
 	marginTop += 100;
-	drawText(marginLeft, marginTop, 'Month: ' + percent.toFixed(2) + '%');
+	drawText(marginLeft, marginTop, 'Month: ' + percent.toFixed(2) + '%', '#F5F5F5');
 	marginTop += 20;
 	drawProgress(marginLeft, marginTop, Math.round(percent), colors[1]);
 
@@ -127,7 +146,7 @@ function animate() {
 	percent = passedDaysFromBegginigOfTheYear * 100 / date.getDaysInYear();
 
 	marginTop += 100;
-	drawText(marginLeft, marginTop, 'Year: ' + percent.toFixed(2) + '%');
+	drawText(marginLeft, marginTop, 'Year: ' + percent.toFixed(2) + '%', '#F5F5F5');
 	marginTop += 20;
 	drawProgress(marginLeft, marginTop, Math.round(percent), colors[2]);
 }
@@ -140,56 +159,4 @@ function randomIntFromRange(min, max) {
 
 function randomColorFromArray(colors) {
 	return colors[Math.floor(Math.random() * colors.length)];
-}
-
-function getDistance(x1, y1, x2, y2) {
-	let xDistance = x2 - x1;
-	let yDistance = y2 - y1;
-
-	return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
-}
-
-function rotate(velocity, angle) {
-	const rotatedVelocities = {
-		x: velocity.x * Math.cos(angle) - velocity.y * Math.sin(angle),
-		y: velocity.x * Math.sin(angle) + velocity.y * Math.cos(angle)
-	};
-
-	return rotatedVelocities;
-}
-
-function resolveCollision(particle, otherParticle) {
-	const xVelocityDiff = particle.velocity.x - otherParticle.velocity.x;
-	const yVelocityDiff = particle.velocity.y - otherParticle.velocity.y;
-
-	const xDist = otherParticle.x - particle.x;
-	const yDist = otherParticle.y - particle.y;
-
-	if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-		const angle = -Math.atan2(otherParticle.y - particle.y, otherParticle.x - particle.x);
-
-		const m1 = particle.mass;
-		const m2 = otherParticle.mass;
-
-		const u1 = rotate(particle.velocity, angle);
-		const u2 = rotate(otherParticle.velocity, angle);
-
-		const v1 = {
-			x: u1.x * (m1 - m2) / (m1 + m2) + u2.x * 2 * m2 / (m1 + m2),
-			y: u1.y
-		};
-		const v2 = {
-			x: u2.x * (m1 - m2) / (m1 + m2) + u1.x * 2 * m2 / (m1 + m2),
-			y: u2.y
-		};
-
-		const vFinal1 = rotate(v1, -angle);
-		const vFinal2 = rotate(v2, -angle);
-
-		particle.velocity.x = vFinal1.x;
-		particle.velocity.y = vFinal1.y;
-
-		otherParticle.velocity.x = vFinal2.x;
-		otherParticle.velocity.y = vFinal2.y;
-	}
 }
